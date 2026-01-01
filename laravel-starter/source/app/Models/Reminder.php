e36366ac7a4944d91a4ef8fba8820b976fcf9cc5
Calvin Prajogo <calprajogo@gmail.com>
2026-01-01 14:30:11 -0600

    Configure Reminder model to handle table fields

--- laravel-starter/source/app/Models/Reminder.php
+++ laravel-starter/source/app/Models/Reminder.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Reminder extends Model
 {
     use HasFactory;
+
+
+    // Which attributes are mass assignable
+    protected $fillable = [
+        'title',
+        'description',
+        'recurrence_type',
+        'recurrence_interval',
+        'recurrence_days',
+        'reminder_time',
+        'start_date',
+        'end_date',
+    ];
+
+
+    // Tells laravel how to convert database values to PHP types
+    protected function casts(): array
+    {
+        return [
+            'start_date' => 'datetime',
+            'end_date' => 'datetime',
+            'recurrence_days' => 'array',
+        ];
+    }
 }
